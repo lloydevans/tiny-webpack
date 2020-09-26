081acdd92b87181f1f06eb1d3c4a7b3a6fded310
Lloyd Evans <contact@lloydevans.co.uk>
2020-09-26 19:51:42 +0100

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env = {}) => {
+    /**
+     * Build options.
+     */
+    env = { ...{ prod: false }, ...env };
+
+    /**
+     * @type {import("webpack").Configuration}
+     */
+    let config = {
+        entry: './src/index.js',
+
+        output: {
+            filename: 'bundle.js',
+            path: path.join(__dirname, 'build'),
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+
+        plugins: [
+            // Auto generate HTML
+            new HtmlWebpackPlugin(),
+        ],
+
+        mode: env.prod ? 'production' : 'development',
+    };
+
+    return config;
+};
